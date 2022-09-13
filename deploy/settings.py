6385bdcbfc1c8c5bd04dbc248807ea0291bda865
Brian Kimathi <kimathi.brian@eclectics.io>
2022-09-13 10:52:31 +0300

    work

--- deploy/settings.py
+++ deploy/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$7ezyy_152^rx!$qb05bjfm+eq@o53sm$r*e%dsmhkogq&xqa%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','agtechcropdisease.herokuapp.com']
 
